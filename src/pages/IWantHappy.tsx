6a980af26fd84fa7b34cf6fa35b1335c29e21e4d
joshyeom <jhyeom95@daum.net>
2024-10-10 16:37:48 +0900

    i-want-happy 페이지 구현

--- src/pages/IWantHappy.tsx
+++ src/pages/IWantHappy.tsx
@@ -1,11 +1,18 @@
 import Header from "../components/Header"
-import { mainStyle } from "../styles/styles.css"
-
+import Image from "../components/Image"
+import ToTop from "../components/ToTop"
+import { gridMainStyle } from "../styles/styles.css"
 const IWantHappy = () => {
+    const imageUrl = "https://cdn.myportfolio.com/ee8d030c-c182-4be1-bb0a-af801c398dd7/f979998c-64c7-4e3d-be24-9ed79749c574_carw_3x4x640.gif?h=d1e2a697b2276b192cce740dcdc1a151"
+    const virtualArray = new Array(20).fill(0)
     return(
-        <div style={{overflow: "hidden"}}>
+        <div style={{overflow: "hidden", position: "relative"}}>
             <Header/>
-            <main className={mainStyle}>
+            <ToTop/>
+            <main className={gridMainStyle}>
+                {
+                    virtualArray.map((v, i) => <Image url={imageUrl} idx={i} key={v}/>)
+                }
             </main>
         </div>
     )
--- src/styles/styles.css.ts
+++ src/styles/styles.css.ts
@@ -1,59 +1,112 @@
-import { style } from "@vanilla-extract/css";
+import { style } from '@vanilla-extract/css';
 
 export const mainStyle = style({
-  width: "100vw",
-  height: "100vh",
-  overflow: "hidden",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  padding: "20px",
-  paddingLeft: "40px",
+  width: '100vw',
+  height: '100vh',
+  overflow: 'hidden',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  padding: '20px',
+  paddingLeft: '40px',
 });
 
 export const sectionStyle = style({
-  width: "100%",
-  height: "100%",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "flex-start",
-  paddingBottom: "88px",
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  paddingBottom: '88px',
 });
 
 export const buttonStyle = style({
-  borderRadius: "100px",
-  color: "#000000",
-  fontSize: "100px",
+  borderRadius: '100px',
+  color: '#000000',
+  fontSize: '100px',
   fontWeight: 700,
-  lineHeight: "95px",
-  textTransform: "none",
-  backgroundColor: "white",
-  border: "none",
-  transition: "background 0.2s ease, color 0.2s ease",
-  padding: "0 30px 7px 30px",
-  ":hover": {
-    backgroundColor: "#0b00ff",
-    color: "#ffffff",
+  lineHeight: '95px',
+  textTransform: 'none',
+  backgroundColor: 'white',
+  border: 'none',
+  transition: 'background 0.2s ease, color 0.2s ease',
+  padding: '0 30px 7px 30px',
+  ':hover': {
+    backgroundColor: '#0b00ff',
+    color: '#ffffff',
   },
-  ":focus": {
-    outline: "none",
+  ':focus': {
+    outline: 'none',
   },
 });
 
 export const headerStyle = style({
-  WebkitAlignItems: "center",
-  display: "flex",
-  width: "100vw",
-  alignItems: "center",
-  height: "515px",
-  paddingBottom: "115px",
-  paddingLeft: "5.5%",
-  paddingRight: "6%",
-  paddingTop: "300px",
-  overflow: "hidden",
+  WebkitAlignItems: 'center',
+  display: 'flex',
+  width: '100vw',
+  alignItems: 'center',
+  height: '515px',
+  paddingBottom: '230px',
+  paddingLeft: '5.5%',
+  paddingRight: '6%',
+  paddingTop: '300px',
+  overflow: 'hidden',
 });
 
 export const headerButtonStyle = style({
-  paddingRight: "20px",
+  paddingRight: '20px',
+});
+
+export const gridMainStyle = style({
+  padding: '100px 0',
+  display: 'flex',
+  flexWrap: 'wrap',
+});
+
+export const cardStyle = style({
+  position: 'relative',
+  width: '303.5px',
+  height: '345.61px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  cursor: 'pointer',
+});
+
+export const cardHoverStyle = style({
+  position: 'absolute',
+  height: '200px',
+  width: '150.6px',
+  backgroundColor: 'rgba(34, 34, 34, 0.8)',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  opacity: 0,
+  zIndex: 3,
+  transition: 'opacity 0.15s ease',
+});
+
+export const cardHoverVisibleStyle = style({
+  opacity: 1,
+});
+
+export const toTopStyle = style({
+  background: 'rgba(0, 0, 0, 0.1)',
+  borderRadius: '60px',
+  color: 'rgba(0, 0, 0, 0.3)',
+  height: '60px',
+  width: '60px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  bottom: '40px',
+  right: '40px',
+  position: 'fixed',
+  zIndex: 3,
+  transition: 'background 0.2s ease, color 0.2s ease',
+  ':hover': {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+  },
 });
