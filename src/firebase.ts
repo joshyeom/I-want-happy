0d1056f75875041b5f9d1ec73e50aa862bea1268
joshyeom <jhyeom95@daum.net>
2024-10-10 21:54:45 +0900

    feat: fetchImage 구현

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// firebase.js 또는 firebaseConfig.js와 같은 파일에서
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app); // Firebase Storage 초기화
