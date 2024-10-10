ea75ca9dabee2200d4133de6dcfae49140830e12
joshyeom <jhyeom95@daum.net>
2024-10-10 22:00:36 +0900

    fetch image 구현

---
+++ src/utils/fetchImage.ts
@@ -0,0 +1,21 @@
+import { storage } from '../firebase';
+import { getDownloadURL, listAll, ref } from 'firebase/storage';
+
+export const fetchImage = async (folderPath: string): Promise<string[]> => {
+  try {
+    const folderRef = ref(storage, `${folderPath}`);
+    console.log(folderRef);
+    const res = await listAll(folderRef);
+
+    const urls = await Promise.all(
+      res.items.map(async (itemRef) => {
+        const url = await getDownloadURL(itemRef);
+        return url;
+      })
+    );
+    return urls;
+  } catch (error) {
+    console.error('Error fetching images: ', error);
+    return [];
+  }
+};
