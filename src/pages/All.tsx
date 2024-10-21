6ff6cedbf837e9a62cd29f2935bde9e437cf32b1
joshyeom <jhyeom95@daum.net>
2024-10-21 15:50:14 +0900

    refactor: All 페이지에서 origin 페이지 렌더링 및 컴포넌트 분리

--- src/pages/All.tsx
+++ src/pages/All.tsx
@@ -1,9 +1,9 @@
 import { useParams } from "react-router-dom"
 import Header from "../components/Header"
 import { useState } from "react";
-import Modal from "../components/Modal";
+import OneModal from "../components/OneModal";
 import { useFetchMultipleImages } from "../hooks/useFetchMultipleImages";
-// import Card from "../components/Card";
+import MiniCard from "../components/MiniCard";
 
 const All = () => {
     const { url } = useParams<{ url: string}>();
@@ -13,10 +13,10 @@ const All = () => {
     const [isModalOpen, setModalOpen] = useState(false);
     const [selectedImgUrl, setSelectedImgUrl] = useState<string | null>(null);
 
-    // const openModal = (url: string) => {
-    //     setSelectedImgUrl(url); 
-    //     setModalOpen(true);
-    // };
+    const openModal = (url: string) => {
+        setSelectedImgUrl(url); 
+        setModalOpen(true);
+    };
 
     const closeModal = () => {
         setSelectedImgUrl(null);
@@ -25,12 +25,13 @@ const All = () => {
 
     const isLoading = queries.some(query => query.isLoading);
     const hasError = queries.some(query => query.error);
-    
+
     if (isLoading) return <div>Loading...</div>;
     if (hasError) return <div>Error loading images</div>;
 
+    const originImages = queries[0]?. data || [];
     const resizedImages = queries[1]?.data || [];
-    
+
     return(
         <>
             <Header currentPage={url!}/>
@@ -40,35 +41,26 @@ const All = () => {
                 </section>
                 <section style={{display: 'flex', alignItems: 'center'}}>
                     <section style={{
-                        width: '810px',
-                        height: '90vh',
-                        display: 'flex', 
-                        flexWrap: 'wrap',
-                        gap: '10px',
+                        width: '600px',
+                        height: '95vh',
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(10, 1fr)',
+                        gridAutoRows: '2fr',
+                        gap: '10px'     
                     }}>
                 {
                 // data가 100개가 되도록 하기 위해 필요한 대체 이미지의 수를 계산
-                Array.from({ length: 100 }).map((_, i) => {
-                    const imageUrl = resizedImages && i < resizedImages.length ? resizedImages[i] : ''; // 대체 이미지 URL
-                    return (
-                            <img 
-                                src={imageUrl} 
-                                alt={`Image ${i + 1}`} 
-                                key={i} 
-                                style={{
-                                    width: '8.8%',
-                                    height: '8.8%',
-                                    objectFit: 'fill', 
-                                    flex: '0 0 auto'
-                            }}/>
-                            );
-                        })
+                    Array.from({ length: 100 }).map((_, i) => {
+                        const resizedImage = resizedImages && i < resizedImages.length ? resizedImages[i] : ''; // 대체 이미지 URL
+                        const originImage = originImages && i < originImages.length ? originImages[i] : ''; // 대체 이미지 URL
+                        return <MiniCard resizedImage={resizedImage} originImage={originImage} index={i} openModal={openModal}/>
+                    })
                 }
                     </section>
                 </section>
             </main>
             {isModalOpen && selectedImgUrl && (
-              <Modal onClose={closeModal} imgUrl={selectedImgUrl} />
+              <OneModal onClose={closeModal} imgUrl={selectedImgUrl} />
             )}
         </>
     )
