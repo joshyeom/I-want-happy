c7deeb87ec1aef359b81e9f6773f3146702d126b
joshyeom <jhyeom95@daum.net>
2024-10-28 15:32:37 +0900

    fix: twomodal 동시에 뜨게 변경

--- src/components/TwoModal.tsx
+++ src/components/TwoModal.tsx
@@ -4,8 +4,9 @@ import { ModalContentsStyle, ModalImageStyle, ModalStyle } from '../styles/compo
 import Skeleton from './Skeleton';
 
 const TwoModal = ({ onClose, imgUrl, kakaoUrl }: {onClose: () => void, imgUrl: string, kakaoUrl: string}) => {
-    const [imgLoaded, setImgLoaded] = useState(false)
-    const [kakaoLoaded, setKakaoLoaded] = useState(false)
+    const [imgLoaded, setImgLoaded] = useState(false);
+    const [kakaoLoaded, setKakaoLoaded] = useState(false);
+    const [bothImagesLoaded, setBothImagesLoaded] = useState(false);
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
@@ -15,16 +16,22 @@ const TwoModal = ({ onClose, imgUrl, kakaoUrl }: {onClose: () => void, imgUrl: s
         };
     }, []);
 
+    useEffect(() => {
+        if (imgLoaded && kakaoLoaded) {
+            setBothImagesLoaded(true);
+        }
+    }, [imgLoaded, kakaoLoaded]);
+
     return createPortal(
         <div className={ModalStyle} onClick={onClose} style={{justifyContent: 'center'}}>
             <div className={ModalContentsStyle}>
-                {!imgLoaded && (
+                {!bothImagesLoaded && (
                     <Skeleton/>
                 )}
                     <img src={imgUrl} alt="Modal Content" onLoad={() => setImgLoaded(true)} className={ModalImageStyle}/>
             </div>
             <div className={ModalContentsStyle}>
-                {!kakaoLoaded && (
+                {!bothImagesLoaded && (
                     <Skeleton/>
                 )}
                     <img src={kakaoUrl} alt="Kakao Content" onLoad={() => setKakaoLoaded(true)} className={ModalImageStyle}/>
